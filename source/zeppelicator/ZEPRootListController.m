7d03b74c99a0a7a79307f26d686f250b61f22131
qwertyuiop1379 <thoffman8839@gmail.com>
2018-08-18 12:24:16 -0700

    Updated Zeppelicator

--- source/zeppelicator/ZEPRootListController.m
+++ source/zeppelicator/ZEPRootListController.m
@@ -2,24 +2,20 @@
 #import <CoreGraphics/CoreGraphics.h>
 
 UIImage *icon = nil;
-UITextField *iconNameField;
     
 @implementation UIImage (scale)
 -(UIImage *)scaleToSize:(CGSize)size
 {
     UIGraphicsBeginImageContext(size);
-  
     [self drawInRect:CGRectMake(0, 0, size.width, size.height)];
- 
     UIImage* scaledImage = UIGraphicsGetImageFromCurrentImageContext();
- 
     UIGraphicsEndImageContext();
-
     return scaledImage;
 }
 @end
     
 @implementation ZEPRootListController
+    
 - (NSArray *)specifiers
 {
 	if (!_specifiers)
@@ -40,45 +36,44 @@ UITextField *iconNameField;
 
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
 {
-    if ([icon isEqual:nil])
+    icon = [info objectForKey:UIImagePickerControllerOriginalImage];
+    if (!icon)
     {
-        [[[UIAlertView alloc] initWithTitle:@"Error" message:@"An error occurred. Please try again." delegate:nil cancelButtonTitle:@"Okay" otherButtonTitles:nil, nil] show];
+        [[[UIAlertView alloc] initWithTitle: @"Error" message:@"An error occurred. Please try again." delegate:nil cancelButtonTitle:@"Okay" otherButtonTitles:nil, nil] show];
+        return;
     }
-    else
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    UIAlertController * alertController = [UIAlertController alertControllerWithTitle: @"Zeppelicator" message: @"Name icon" preferredStyle:UIAlertControllerStyleAlert];
+    [alertController addTextFieldWithConfigurationHandler:^(UITextField *textField)
     {
-        icon = [info objectForKey:UIImagePickerControllerOriginalImage];
-        [picker dismissViewControllerAnimated:YES completion:nil];
-        UIAlertController * alertController = [UIAlertController alertControllerWithTitle: @"Zeppelicator" message: @"Name icon" preferredStyle:UIAlertControllerStyleAlert];
-        [alertController addTextFieldWithConfigurationHandler:^(UITextField *textField)
+        textField.placeholder = @"Name";
+        textField.textColor = [UIColor blueColor];
+        textField.clearButtonMode = UITextFieldViewModeWhileEditing;
+        textField.borderStyle = UITextBorderStyleRoundedRect;
+    }];
+    [alertController addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action){}]];
+    [alertController addAction:[UIAlertAction actionWithTitle:@"Okay" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action)
+    {
+        NSArray *textfields = alertController.textFields;
+        UITextField *namefield = textfields[0];
+        NSString *path = @"/Library/Zeppelin/";
+        path = [path stringByAppendingString:namefield.text];
+        BOOL isDir = NO;
+        if ([[NSFileManager defaultManager] fileExistsAtPath:path isDirectory:&isDir])
         {
-            textField.placeholder = @"Name";
-            textField.textColor = [UIColor blueColor];
-            textField.clearButtonMode = UITextFieldViewModeWhileEditing;
-            textField.borderStyle = UITextBorderStyleRoundedRect;
-        }];
-        [alertController addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action)
+            [[[UIAlertView alloc] initWithTitle:namefield.text message:@"An icon with that name already exists." delegate:nil cancelButtonTitle:@"Okay" otherButtonTitles:nil, nil] show];
+        }
+        else
         {
-            NSArray *textfields = alertController.textFields;
-            UITextField *namefield = textfields[0];
-            NSString *path = @"/Library/Zeppelin/";
-            path = [path stringByAppendingString:namefield.text];
-            BOOL isDir = NO;
-            if ([[NSFileManager defaultManager] fileExistsAtPath:path isDirectory:&isDir])
-            {
-                [[[UIAlertView alloc] initWithTitle:namefield.text message:@"An icon with that name already exists." delegate:nil cancelButtonTitle:@"Okay" otherButtonTitles:nil, nil] show];
-            }
-            else
-            {
-                const char *cmd = (const char *)[[@"mkdir " stringByAppendingString:path] UTF8String];
-                system(cmd);
-                icon = [icon scaleToSize:CGSizeMake(icon.size.width / (icon.size.height/40), icon.size.height / (icon.size.height / 40))];
-                [UIImagePNGRepresentation(icon) writeToFile:[path stringByAppendingString:@"/black@2x.png"] atomically:YES];
-                [UIImagePNGRepresentation(icon) writeToFile:[path stringByAppendingString:@"/etched@2x.png"] atomically:YES];
-                [UIImagePNGRepresentation(icon) writeToFile:[path stringByAppendingString:@"/silver@2x.png"] atomically:YES];
-                [[[UIAlertView alloc] initWithTitle:namefield.text message:@"Icon created. To enable it, select it from the list of themes in Zeppelin settings." delegate:nil cancelButtonTitle:@"Okay" otherButtonTitles:nil, nil] show];
-            }
-        }]];
-        [self presentViewController:alertController animated:YES completion:nil];
-    }
+            const char *cmd = (const char *)[[@"mkdir " stringByAppendingString:path] UTF8String];
+            system(cmd);
+            icon = [icon scaleToSize:CGSizeMake(icon.size.width / (icon.size.height/40), icon.size.height / (icon.size.height / 40))];
+            [UIImagePNGRepresentation(icon) writeToFile:[path stringByAppendingString:@"/black@2x.png"] atomically:YES];
+            [UIImagePNGRepresentation(icon) writeToFile:[path stringByAppendingString:@"/etched@2x.png"] atomically:YES];
+            [UIImagePNGRepresentation(icon) writeToFile:[path stringByAppendingString:@"/silver@2x.png"] atomically:YES];
+            [[[UIAlertView alloc] initWithTitle:namefield.text message:@"Icon created. To enable it, select it from the list of themes in Zeppelin settings." delegate:nil cancelButtonTitle:@"Okay" otherButtonTitles:nil, nil] show];
+        }
+    }]];
+    [self presentViewController:alertController animated:YES completion:nil];
 }
 @end
