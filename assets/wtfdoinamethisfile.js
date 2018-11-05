75915f68d1c08ac4ad795f845cf4ff10322c0aaa
qwertyuiop1379 <thoffman8839@gmail.com>
2018-11-04 22:54:44 -0700

    Styled page properly, +more
    Added css and js
    Added packages and ability to search for them
    Removed clearpasskeys, it is now deprecated with the release of RomanKeypad
    Website looks much cleaner now

---
+++ assets/wtfdoinamethisfile.js
@@ -0,0 +1,38 @@
+function trimresults()
+{
+	var input, filter, table, tr, td, i, j, tds, ths, matched;
+    input = document.getElementById("searchpackages");
+    filter = input.value.toUpperCase();
+    tr = document.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++)
+	{
+        tds = tr[i].getElementsByTagName("td");
+        ths = tr[i].getElementsByTagName("th");
+        matched = false;
+        if (ths.length > 0)
+		{
+            matched = true;
+        }
+        else
+		{
+            for (j = 0; j < tds.length; j++)
+			{
+                td = tds[j];
+                if (td.innerHTML.toUpperCase().indexOf(filter) > -1)
+				{
+                    matched = true;
+                    break;
+                }
+
+            }
+        }
+        if (matched == true)
+		{
+            tr[i].style.display = "";
+        }
+        else
+		{
+            tr[i].style.display = "none";
+        }
+	}
+}
