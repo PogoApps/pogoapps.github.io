75b18cb6b02b929b7e0cb2424533d2ea2cee992f
qwertyuiop1379 <thoffman8839@gmail.com>
2018-12-11 14:50:57 -0700

    removed source code, moved to ../tweaks

--- source/designerxi/Application/DXIAppDelegate.h
+++
@@ -1,4 +0,0 @@
-@interface DXIAppDelegate : UIResponder <UIApplicationDelegate>
-@property (nonatomic, retain) UIWindow *window;
-@property (nonatomic, retain) UINavigationController *rootViewController;
-@end
--- source/zeppelicator/ZEPRootListController.h
+++
@@ -1,8 +0,0 @@
-#import <Preferences/PSListController.h>
-
-@interface UIImage (scale)
--(UIImage *)scaleToSize:(CGSize)size;
-@end
-
-@interface ZEPRootListController : PSListController <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
-@end
